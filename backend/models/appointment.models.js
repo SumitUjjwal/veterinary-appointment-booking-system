53cf79252ba4d270efba18b57a10cee585b8d82c
SumitUjjwal <sumitonboard@gmail.com>
2023-03-29 00:28:28 +0530

    appointment schema

--- backend/models/appointment.models.js
+++ backend/models/appointment.models.js
@@ -7,11 +7,33 @@
 //         - Phone
 //         - location
 //         - Description
+// petIssues - {
+    //  General Medical Question
+    //  Diarrhea or bowel issues
+    //  Ear infection
+    //  Loss of appetite
+    //  Throwing up
+    //  Behavioural problems
+    //  Skin rash or allergy
+    //  Injury
+    //  Dental issues
+    //  Other
+// }
 
 const mongoose = require("mongoose");
 
 const appointmentSchema = mongoose.Schema({
     DoctorId: {type: String, required: true},
-    PetOwnerId: {type: String, required: true},
-    
-})
+    ownerName: {type: String, required: true},
+    ownerEmail: {type: String, required: true},
+    ownerPhone: {type: Number, required: true},
+    petCategory: {type: String, required: true},
+    petIssue: {type: String, required: true},
+    appointmentDate: {type: String, required: true}
+});
+
+const AppointmentModel = mongoose.model("appointment", appointmentSchema);
+
+module.exports = {
+    AppointmentModel
+}
