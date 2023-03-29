cc4f8afca36520cbdc6ff2b0b074323cd8b8d46c
SumitUjjwal <sumitonboard@gmail.com>
2023-03-29 17:41:52 +0530

    create: admin register and login

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -4,3 +4,65 @@
 // add new doctor
 // remove a doctor
 // update a doctor info
+
+// --------------------External Modules --------------------------------
+const express = require('express');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+// --------------------Custom Module --------------------------------
+const { AdminModel } = require("../models/admin.model");
+
+
+const adminRouter = express.Router();
+adminRouter.use(express.json());
+
+adminRouter.get("/", async (req, res) => {
+    res.json({ "msg": "Admin Routes" });
+})
+
+adminRouter.post("/register", async (req, res) => {
+    const { first_name, last_name, email, phone, password } = req.body;
+    const checkExisting = await AdminModel.findOne({ email });
+    if (checkExisting) {
+        res.json({ "msg": "Already registered" });
+    }
+    else {
+        bcrypt.hash(password, 2, async (err, hash) => {
+            console.log(hash)
+            if (hash) {
+                const user = new AdminModel({ first_name, last_name, email, phone, password: hash });
+                await user.save();
+                res.json({ "msg": `${first_name} has been registered successfully!!` });
+            }
+            else {
+                res.json({ "msg": err });
+            }
+        })
+    }
+});
+
+adminRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await AdminModel.findOne({ email });
+        console.log(user);
+        if (user) {
+            bcrypt.compare(password, user.password, async (err, result) => {
+                if (result) {
+                    res.json({ "msg": `${user.first_name} has logged in successfully!!`, "admin": user._id });
+                }
+                else {
+                    res.json({ "msg": "Invalid credentials", "error": err });
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({ "msg": "Error while admin login", "error": error })
+    }
+});
+
+module.exports = {
+    adminRouter
+}
