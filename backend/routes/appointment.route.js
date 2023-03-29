4fb696f600b340f244c563d71e21b1df6d84f174
SumitUjjwal <sumitonboard@gmail.com>
2023-03-29 15:17:07 +0530

    create: create appointment

--- backend/routes/appointment.route.js
+++ backend/routes/appointment.route.js
@@ -1,1 +1,32 @@
 // create a new appointment 
+
+// --------------------External Modules --------------------------------
+const express = require('express');
+
+// --------------------Custom Module --------------------------------
+const { AppointmentModel } = require("../models/appointment.models");
+
+
+const appointmentRouter = express.Router();
+appointmentRouter.use(express.json());
+
+appointmentRouter.get("/", async(req,res) => {
+    res.json({"msg": "Appointment Routes"});
+})
+
+appointmentRouter.post("/create", async (req, res) => {
+    const { doctorId, ownerName, ownerEmail, ownerPhone, petCategory, petIssue, appointmentDate } = req.body;
+    try {
+        const appointment = new AppointmentModel({ doctorId, ownerName, ownerEmail, ownerPhone, petCategory, petIssue, appointmentDate });
+        await appointment.save();
+        res.json({ "msg": "Appointment Created successfully" });
+    } catch (error) {
+        console.log(error);
+        request.json({ "msg": "Error in creating appointment"});
+    }
+})
+
+
+module.exports = {
+    appointmentRouter
+}
