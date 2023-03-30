7ad76e7b812ee928ce66c0e9c4219ff45611960f
SumitUjjwal <sumitonboard@gmail.com>
2023-03-30 11:54:35 +0530

    update: profile image

--- backend/models/doctors.model.js
+++ backend/models/doctors.model.js
@@ -20,6 +20,7 @@
 const mongoose = require("mongoose");
 
 const doctorSchema = mongoose.Schema({
+    profile_img: { type: String, default: "https://th.bing.com/th?id=OIP.cububKdcQPlH2Gpvs3EidgAAAA&w=250&h=250&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2" },
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     specialization: { type: String, required: true },
