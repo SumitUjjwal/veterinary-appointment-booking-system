a7eca72fd3480bf6d954767a1ad92de62260f033
SumitUjjwal <sumitonboard@gmail.com>
2023-03-30 20:52:29 +0530

    create: update doctor info in db by id

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -137,7 +137,14 @@ adminRouter.get("/appointments", async (req, res) => {
 
 adminRouter.patch("/updateDoctor/:id", async(req, res) => {
     const payload = req.body;
-
+    const id = req.params.id;
+    try {
+        const doctor = await DoctorModel.findByIdAndUpdate(id, payload);
+        res.json({"msg": `Doctor info associated with id: ${id} has been updated successfully`})
+    } catch (error) {
+        console.log(error);
+        res.json({"Error": error.message});
+    }
 });
 
 
