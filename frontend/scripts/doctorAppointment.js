e4536b084f1dab6276f2596066893eaf8c5d44fa
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-03-30 12:08:59 +0530

    file structre redifined

--- frontend/scripts/doctorAppointment.js
+++ frontend/scripts/doctorAppointment.js
@@ -1,3 +1,6 @@
 import Navbar from "./components/navbar.component.js";
 let header = document.querySelector("header");
 header.innerHTML = Navbar();
+document.querySelector("#logo").onclick = ()=>{
+    location.href = "../index.html"
+}
