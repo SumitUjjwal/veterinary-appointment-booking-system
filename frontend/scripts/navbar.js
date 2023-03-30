2c219a6f0420c102dccfe16192db58d639401e94
Pranjal Jain <pranjaljain275@gmail.com>
2023-03-30 18:48:15 +0530

    day3 work fw22_0187

--- frontend/scripts/navbar.js
+++ frontend/scripts/navbar.js
@@ -1,12 +1,4 @@
-let header = document.querySelector("header");
-
-header.innerHTML = `
-    <div class="navbar">
-        <img src="../assets/images/Pet-Vet logo.png" alt="" />
-        <a href="#">Home</a>
-        <a href="#">Category</a>
-        <a href="#">Appointment</a>
-        <a href="#">Doctor Apply</a>
-        <a href="#">DashBoard</a>
-    </div>
-`;
+let logo = document.querySelector(".nvbr img");
+logo.addEventListener("click",()=>{
+    window.location.href = "index.html";
+});
