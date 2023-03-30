ba1bc834acee95b350f435d33776237538f76b51
SumitUjjwal <sumitonboard@gmail.com>
2023-03-30 20:23:34 +0530

    create: remove doctor info from db by id

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -135,10 +135,23 @@ adminRouter.get("/appointments", async (req, res) => {
 
 // *************************UPDATE DOCTOR INFO*************************
 
+adminRouter.patch("/updateDoctor/:id", async(req, res) => {
+    const payload = req.body;
+
+});
 
 
 // *************************REMOVE A DOCTOR*************************
-
+adminRouter.delete("/removeDoctor/:id", async(req, res) => {
+    const id = req.params.id;
+    try {
+        const doctor = await DoctorModel.findByIdAndDelete({id});
+        res.json({"msg": `Doctor info associated with id: ${id} has been removed successfully`})
+    } catch (error) {
+        console.log(error);
+        res.json({"Error": error.message});
+    }
+});
 
 // *************************EXPORT*************************
 
