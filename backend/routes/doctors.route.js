e405dd80badc24b44c7366e6bba1b34188e269e7
SumitUjjwal <sumitonboard@gmail.com>
2023-03-31 00:14:31 +0530

    fix: error while accessing doctor route

--- backend/routes/doctors.route.js
+++ backend/routes/doctors.route.js
@@ -4,31 +4,34 @@
 // update the appointment status - confirm, cancel and completed - after updating send mail to petOwner
 
 
-
-const {DoctorModel}= require("../models/doctors.model");
+const { DoctorModel } = require("../models/doctors.model");
 const express = require("express")
 require("dotenv").config();
 const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken");
 
-const DoctorRouter = express.Router();
-DoctorRouter.use(express.json());
+const doctorRouter = express.Router();
+doctorRouter.use(express.json());
 
-DoctorRouter.post("/register",async(req,res)=>{
-    const { first_name,last_name,specialization,experience,address,phone,fees, email, password } = req.body;
+doctorRouter.get("/", async (req, res) => {
+    res.json({ "msg": "Doctor Route" });
+})
+
+doctorRouter.post("/register", async (req, res) => {
+    const { first_name, last_name, specialization, experience, address, phone, fees, email, password } = req.body;
     try {
-        let all_data = await DoctorModel.find({email});
-        if(all_data.length === 0){
-            bcrypt.hash(password, 5,async(err,val)=>{
-                if(err){
+        let all_data = await DoctorModel.find({ email });
+        if (all_data.length === 0) {
+            bcrypt.hash(password, 5, async (err, val) => {
+                if (err) {
                     res.send("login is not working")
-                }else{
-                    const Doctor = new DoctorModel({first_name,last_name,specialization,experience,address,phone,fees, email, password:val });
+                } else {
+                    const Doctor = new DoctorModel({ first_name, last_name, specialization, experience, address, phone, fees, email, password: val });
                     await Doctor.save()
                     res.send("Doctor registered Successfully")
                 }
             })
-        }else{
+        } else {
             res.send("Doctor already Regester")
         }
     } catch (error) {
@@ -38,21 +41,21 @@ DoctorRouter.post("/register",async(req,res)=>{
 })
 
 
-DoctorRouter.post("/login",async(req,res)=>{
-    const {email,password}=req.body;
+doctorRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
     try {
-        const Doctor = await DoctorModel.find({email});
+        const Doctor = await DoctorModel.find({ email });
         const hashed_pass = Doctor[0].password;
-        if(Doctor.length>0){
-            bcrypt.compare(password,hashed_pass,(err,result)=>{
-                if(result){
-                    const token = jwt.sign({Doctorid:Doctor[0]._id},process.env.secret);
-                    res.send({"msg":"Login Successfull","Access_Token":token})
-                }else{
+        if (Doctor.length > 0) {
+            bcrypt.compare(password, hashed_pass, (err, result) => {
+                if (result) {
+                    const token = jwt.sign({ Doctorid: Doctor[0]._id }, process.env.secret);
+                    res.send({ "msg": "Login Successfull", "Access_Token": token })
+                } else {
                     res.send("Wrong Credntials")
                 }
             })
-        }else{
+        } else {
             res.send("Doctor Not registered")
         }
     } catch (error) {
@@ -60,6 +63,6 @@ DoctorRouter.post("/login",async(req,res)=>{
     }
 })
 
-module.exports={
-    DoctorRouter
+module.exports = {
+    doctorRouter
 }
