ee85f586f86b890954141d7bb65d89c56d12d014
SumitUjjwal <sumitonboard@gmail.com>
2023-04-02 13:53:00 +0530

    upadte: authentication

--- frontend/scripts/doctorDashboard.js
+++ frontend/scripts/doctorDashboard.js
@@ -12,7 +12,13 @@ document.getElementById("docName").innerHTML = "Dr. " + doctorName;
 
 // *******************Fetch Appointment********************
 const getAppointments = async () => {
-    const request = await fetch(`${baseURL}/doctor/${docId}`);
+    const request = await fetch(`${baseURL}/doctor/${docId}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `${sessionStorage.getItem("accessToken")}`
+        }
+    });
     const response = await request.json();
     displayAppointments(response);
 }
@@ -20,14 +26,26 @@ getAppointments();
 
 // *******************Pending Appointments********************
 const pendingAppointments = async () => {
-    const request = await fetch(`${baseURL}/doctor/${docId}?status=pending`);
+    const request = await fetch(`${baseURL}/doctor/${docId}?status=pending`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `${sessionStorage.getItem("accessToken")}`
+        }
+    });
     const response = await request.json();
     displayAppointments(response);
 }
 
 // *******************Confirm Appointments********************
 const confirmAppointments = async () => {
-    const request = await fetch(`${baseURL}/doctor/${docId}?status=confirm`);
+    const request = await fetch(`${baseURL}/doctor/${docId}?status=confirm`,{
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "authorization": `${sessionStorage.getItem("accessToken")}`
+        }
+    });
     const response = await request.json();
     displayAppointments(response);
 }
@@ -49,7 +67,13 @@ const displayAppointments = async (appointments) => {
                 <td>${appointment.petCategory}</td>
                 <td>${appointment.petIssue}</td>
                 <td><span class="status">${appointment.appointmentStatus}</span></td>
-                <td><a href="#" class="btn-action confirm" id="${appointment._id}" onClick="actionConfirm(event)">Confirm</a><a href="#" class="btn-action reject" id="${appointment._id}" onClick="actionReject(event)">Reject</a></td>
+                <td>
+                    <select class="btn-action" onchange="action(event)">
+                        <option value="">Select</option>
+                        <option value="confirm,${appointment._id}" class="btn-action confirm" id="${appointment._id}" onchange="actionConfirm(event)">Confirm</option>
+                        <option value="reject,${appointment._id}" class="btn-action reject" id="${appointment._id}" onchange="actionReject(event)">Reject</option>
+                    </select>
+                </td>
               </tr>
             `;
         }
@@ -64,7 +88,22 @@ const displayAppointments = async (appointments) => {
                 <td>${appointment.petCategory}</td>
                 <td>${appointment.petIssue}</td>
                 <td><span class="status">${appointment.appointmentStatus}</span></td>
-                <td><a href="#" class="btn-action complete" id="${appointment._id}" style="background-color: green;">Completed</a></td>
+                <td><a href="#" class="btn-action complete" id="${appointment._id}" onclick="action(event)" style="background-color: green;">Mark as Complete</a></td>
+              </tr>
+            `;
+        }
+        else if (appointment.appointmentStatus == "completed") {
+            appointmentRow = `
+              <tr>
+                <td>${appointment._id}</td>
+                <td>${appointment.ownerName}</td>
+                <td>${appointment.ownerEmail}</td>
+                <td>${appointment.ownerPhone}</td>
+                <td>${appointment.appointmentDate}</td>
+                <td>${appointment.petCategory}</td>
+                <td>${appointment.petIssue}</td>
+                <td><span class="status">${appointment.appointmentStatus}</span></td>
+                <td><a href="#" class="btn-action complete" id="${appointment._id}" onclick="action(event)" style="background-color: green;">Completed</a></td>
               </tr>
             `;
         }
@@ -91,10 +130,19 @@ const displayAppointments = async (appointments) => {
 
 // *******************Toggle Appointment Status********************
 
-const actionConfirm = async (event) => {
-    const id = event.target.id;
-    console.log(id);
-    const obj = { appointmentStatus: "confirm" };
+const action = async (event) => {
+    let value, status, id;
+    if (event.target.value) {
+        value = event.target.value.trim().split(",");
+        status = value[0];
+        id = value[1];
+    }
+    else {
+        status = "completed";
+        id = event.target.id;
+    }
+    // console.log(id, status);
+    const obj = { appointmentStatus: status };
     const request = await fetch(`${baseURL}/doctor/status/${id}`, {
         method: "PATCH",
         headers: {
@@ -106,20 +154,37 @@ const actionConfirm = async (event) => {
     window.location.reload();
 }
 
-const actionReject = async (event) => {
-    const id = event.target.id;
-    console.log(id);
-    const obj = { appointmentStatus: "reject" };
-    const request = await fetch(`${baseURL}/doctor/status/${id}`, {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(obj)
-    });
-    const response = await request.json();
-    window.location.reload();
-}
+// const actionConfirm = async (event) => {
+//     console.log("confirmAppointment")
+//     const id = event.target.id;
+//     console.log(id);
+//     const obj = { appointmentStatus: "confirm" };
+//     const request = await fetch(`${baseURL}/doctor/status/${id}`, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(obj)
+//     });
+//     const response = await request.json();
+//     window.location.reload();
+// }
+
+// const actionReject = async (event) => {
+//     console.log("rejectAppointment")
+//     const id = event.target.id;
+//     console.log(id);
+//     const obj = { appointmentStatus: "reject" };
+//     const request = await fetch(`${baseURL}/doctor/status/${id}`, {
+//         method: "PATCH",
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify(obj)
+//     });
+//     const response = await request.json();
+//     window.location.reload();
+// }
 
 
 const logout = () => {
