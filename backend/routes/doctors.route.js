db65a90672a627ff6844c1b3da339649bf799764
SumitUjjwal <sumitonboard@gmail.com>
2023-04-02 10:28:20 +0530

    update: fixed status toggle by doctor

--- backend/routes/doctors.route.js
+++ backend/routes/doctors.route.js
@@ -16,11 +16,17 @@ doctorRouter.use(express.json());
 
 doctorRouter.get("/:id", async (req, res) => {
     const doctorId = req.params.id;
+    const appointmentStatus = req.query.status;
     try {
-        const appointments = await AppointmentModel.find({doctorId});
-        
+        let appointments;
+        if(appointmentStatus){
+            appointments = await AppointmentModel.find({doctorId, appointmentStatus});
+        }
+        else{
+            appointments = await AppointmentModel.find({doctorId});
+        }
         res.json(appointments);
-        console.log(appointments)
+        console.log(appointments);
     } catch (error) {
         console.log(error);
         res.json({"Error": error.message});
@@ -28,17 +34,25 @@ doctorRouter.get("/:id", async (req, res) => {
 });
 
 doctorRouter.patch('/status/:id',async(req, res) => {
-    const doctorId = req.params.id;
-    const status = req.body.status;
-    try{
-        const appointments = await AppointmentModel.find({doctorId});
-        console.log(appointments)
-        if(appointments.length === 0){
-            res.status(404).json({"Error": "No appointments found"});
-        }
-        appointments.status = status
-    }catch(err){console.log(err)} 
-
+    // const id = req.params.id;
+    // const status = req.body.status;
+    // try{
+    //     const appointments = await AppointmentModel.findOne({doctorId});
+    //     console.log(appointments)
+    //     if(appointments.length === 0){
+    //         res.status(404).json({"Error": "No appointments found"});
+    //     }
+    //     appointments.status = status
+    // }catch(err){console.log(err)} 
+    const payload = req.body;
+    const id = req.params.id;
+    try {
+        const appointment = await AppointmentModel.findByIdAndUpdate(id, payload);
+        res.json({ "msg": `Appointment info associated with id: ${id} has been updated successfully` });
+    } catch (error) {
+        console.log(error);
+        res.json({ "Error": error.message });
+    }
 })
 
 module.exports = {
